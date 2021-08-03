a1718fd2d51c2cf05be669cc3db1da40d9dd03ee
Anton <antonbsa@gmail.com>
2021-08-03 01:49:58 -0300

    add progressive logs

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 exports.googleWebsite = 'https://www.google.com.br';
-
+exports.secondsTextPassed = 'second has passed - it takes ~35sec';
 exports.MAX_TIMEOUT_APPLY_CONFIG = 60000;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const ora = require('ora');
 const puppeteer = require('puppeteer');
-const { waitAndClick, waitAndClickElement, waitAndType, getMainFrame, findWifiInTable } = require('./util');
+const { waitAndClick, waitAndClickElement, waitAndType, getMainFrame, findWifiInTable, finishAndSetSpinner } = require('./util');
 const c = require('./constants');
 const e = require('./elements');
 
 (async () => {
+    const debugMode = process.env.DEBUG_MODE === 'true';
     const defaultGatewayIp = process.env.GATEWAY_BASE;
     const newGatewayIp = process.env.NEW_GATEWAY_ADDRESS;
     const wifiName = process.env.WIRELESS_NAME;
@@ -12,9 +14,11 @@ const e = require('./elements');
     const channel = process.env.WIRELESS_CHANNEL;
     const siteSurveyCount = process.env.SITE_SURVEY_COUNT_TRY;
 
+    let spinner = ora('Acessing modem page').start();
+
     try {
         const browser = await puppeteer.launch({
-            headless: true,
+            headless: (debugMode) ? false : true,
             defaultViewport: null
         });
         const page = await browser.newPage();
@@ -25,18 +29,19 @@ const e = require('./elements');
         });
 
         // Enter router configs
+        spinner = finishAndSetSpinner(spinner, 'Entering router configs');
         await waitAndClick(page, e.loginBtn);
         await page.waitForNavigation();
 
         const frame = await getMainFrame(page);
 
-        // Go to wireless tab and set the channel
+        spinner = finishAndSetSpinner(spinner, 'Setting the channel');
         await waitAndClickElement(frame, e.wirelessTab);
         await frame.waitForSelector(e.channelSelect, { visible: true });
         await frame.select(e.channelSelect, channel);
         await waitAndClick(frame, e.saveWirelessTab);
 
-        // Go to wireless repetear tab
+        spinner = finishAndSetSpinner(spinner, 'Going to wireless repetear tab and enabling repeater');
         await waitAndClick(frame, e.wirelessRepeaterTab);
 
         // Enable repeater
@@ -44,19 +49,20 @@ const e = require('./elements');
         const repeaterModeChecked = await frame.evaluate((repeaterModeSelector) => {
             return document.querySelectorAll(repeaterModeSelector)[0].value == 'on';
         }, e.repeaterModeCheckbox);
-        // console.log({ repeaterModeChecked })
+
+        if (debugMode) console.log({ repeaterModeChecked });
         if (!repeaterModeChecked) await waitAndClick(frame, e.repeaterModeCheckbox);
 
-        // repeat N times to find wifi - it takes a while to apply the channel change
         let COUNT_TRY = siteSurveyCount;
         let founded = false;
+        spinner = finishAndSetSpinner(spinner, `Repeating ${COUNT_TRY} times to find wifi - it takes a while to apply the channel change`, 17000);
         while (COUNT_TRY != 0) {
             await waitAndClick(frame, e.siteSurveyButton);
             await frame.waitForTimeout(500);
             await frame.waitForSelector(e.wirelessTableRow, { visible: true });
             founded = await findWifiInTable(frame, e.wirelessTableRow, wifiName, false);
 
-            // console.log({ founded, COUNT_TRY });
+            if (debugMode) console.log({ founded, COUNT_TRY });
             if (founded === true) break;
             COUNT_TRY--;
         };
@@ -64,37 +70,52 @@ const e = require('./elements');
 
         await findWifiInTable(frame, e.wirelessTableRow, wifiName, true);
 
-        // Go next and insert password
+        spinner = finishAndSetSpinner(spinner, 'Inserting password');
         await waitAndClick(frame, e.siteSurveyGoNext);
         await waitAndType(frame, e.wirelessPasswordInput, wifiPassword);
 
-        // Go next and set new IP address
+        spinner = finishAndSetSpinner(spinner, 'Setting new IP address');
         await waitAndClick(frame, e.wirelessPasswordGoNext);
         await waitAndType(frame, e.ipAddressInput, newGatewayIp);
 
         // click to finish
         await waitAndClick(frame, e.finishButton);
 
-        // wait to apply the changes
-        console.log('Waiting to restart the modem and complete the configuration - it takes ~35sec')
-        await page.waitForSelector(e.mainBanner, { timeout: c.MAX_TIMEOUT_APPLY_CONFIG, visible: true });
-        console.log('Settings applied! Testing internet connection');
+        spinner = finishAndSetSpinner(spinner, 'Changes confirmed: waiting to restart the modem and complete the configuration.');
+        spinner = finishAndSetSpinner(spinner, c.secondsTextPassed);
 
-        // final check: go to Google get the response status
+        let interval = 0;
+        const secondsInterval = setInterval(async () => {
+            interval++;
+            spinner.text = `${interval} ${c.secondsTextPassed}`;
+        }, 1000);
+
+        await page.waitForSelector(e.mainBanner, { timeout: c.MAX_TIMEOUT_APPLY_CONFIG, visible: true });
+        clearInterval(secondsInterval);
+
+        spinner.text = 'Changes has been applied!';
+        spinner = finishAndSetSpinner(spinner, 'Testing internet connection');
+
+        // Go to Google and check the response status
         const testPage = await page.goto(c.googleWebsite);
         const status = testPage.status();
 
-        // await page.waitForTimeout(3000);
+        if (debugMode) await page.waitForTimeout(3000);
         if (status == 200) {
             // await page.screenshot({ path: 'google.png' });
-            console.log('Setup completed successfully!');
+            spinner.text = 'Setup completed successfully!';
+            spinner.succeed().stop();
             await browser.close();
             process.exit(0);
         } else {
             await browser.close();
-            throw new Error('Cannot load Google page. Try to access manually and check if the setup works!');
+            spinner.fail().stop();
+            console.error('Cannot load Google page. Try to access manually and check if the setup works!');
+            process.exit(1);
         }
     } catch (e) {
-        throw new Error(e);
+        spinner.fail().stop();
+        console.error(e);
+        process.exit(1);
     }
 })();
